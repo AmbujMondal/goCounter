760b2029cde82d5967e22f191ebcd43ce1db97a6
AmbujMondal <Ambuj.Mondal@outlook.com>
2021-09-01 17:46:52 +0200

    test cases

--- main_test.go
+++ main_test.go
@@ -2,62 +2,82 @@ package main
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func Test_addCounter(t *testing.T) {
-	type args struct {
-		w ResponseWriter
-		r *http.Request
-	}
+
 	tests := []struct {
 		name string
-		args args
+		req  *http.Request
 	}{
 		{
 			name: "increment",
-			args: args{
-				w: nil,
-				r: nil,
-			},
+			req:  httptest.NewRequest("PUT", "/api/counter", nil),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(getCounter)
+			handler.ServeHTTP(rr, tt.req)
+
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+
 		})
 	}
 }
 
 func Test_getCounter(t *testing.T) {
-	type args struct {
-		w ResponseWriter
-		r *http.Request
-	}
+
 	tests := []struct {
 		name string
-		args args
+		req  *http.Request
 	}{
-		// TODO: Add test cases.
+		{
+			name: "getCounter",
+			req:  httptest.NewRequest("GET", "/api/counter", nil),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(getCounter)
+			handler.ServeHTTP(rr, tt.req)
+
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
 		})
 	}
 }
 
 func Test_subCounter(t *testing.T) {
-	type args struct {
-		w ResponseWriter
-		r *http.Request
-	}
+
 	tests := []struct {
 		name string
-		args args
+		req  *http.Request
 	}{
-		// TODO: Add test cases.
+		{
+			name: "decrement",
+			req:  httptest.NewRequest("DELETE", "/api/counter", nil),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(getCounter)
+			handler.ServeHTTP(rr, tt.req)
+
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
 		})
 	}
 }
